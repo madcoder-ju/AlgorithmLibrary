077dcf7ca9b3c90338030bfba6fa813b73a068de
MasumBhuiyan <50369967+MasumBhuiyan@users.noreply.github.com>
2019-09-27 00:13:15 +0600

    Update SegmentTreeLazy.cpp

--- SegmentTreeLazy.cpp
+++ SegmentTreeLazy.cpp
@@ -1,64 +1,72 @@
+/*
+ * LightOJ 1207: Posters for Election
+ * given n line poster as line segments reresented as l r serially
+ * find the number of different posters that are visible
+ * 1 <= l, r <= 2n
+ */
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
 
-const int M = 1e5 + 15;
-int n, l, r;
-ll val;
-ll tree[ M << 2 ];
-ll lazy[ M << 2 ];
+const int N = 2e5 + 15;
 
-void construct(int node = 1, int b = 1, int e = n) {
-    lazy[ node ] = 0;
-    if( b == e ) {
-        tree[ node ] = 0;
-        return ;
-    }
-
-    int m = (b + e) >> 1;
-    construct(node << 1, b, m);
-    construct(node << 1 | 1, m + 1, e);
-
-    tree[ node ] = tree[ node << 1 ] + tree[ node << 1 | 1 ];
-}
+int n, l, r, color, vis[ N << 1 ], tree[ N << 2 ], lazy[ N << 2 ];
 
 void propagate(int node, int b, int e) {
-    if( lazy[ node ] != 0 ) {
-        tree[ node ] += (e - b + 1) * 1LL * lazy[ node ];
+    if( lazy[ node ] ) {
+        tree[ node ] = lazy[ node ];
         if( b != e ) {
-            lazy[ node << 1 ] += lazy[ node ];
-            lazy[ node << 1 | 1 ] += lazy[ node ];
+            lazy[ node << 1 ] = lazy[ node ];
+            lazy[ node << 1 | 1 ] = lazy[ node ];
         }
         lazy[ node ] = 0;
     }
 }
-void update(int node = 1, int b = 1, int e = n) {
+void update(int node = 1, int b = 1, int e = n + n) {
     propagate(node, b, e);
-    if( b > e || b > r || e < l ) return ;
+    if( b > e || b > r || e < l )
+        return ;
     if( l <= b && e <= r ) {
-        lazy[ node ] += val;
+        lazy[ node ] = color;
         propagate(node, b, e);
         return ;
     }
     int m = (b + e) >> 1;
     update(node << 1, b, m);
     update(node << 1 | 1, m + 1, e);
-
     tree[ node ] = tree[ node << 1 ] + tree[ node << 1 | 1 ];
 }
-ll query(int node = 1, int b = 1, int e = n) {
+int query(int node = 1, int b = 1, int e = n + n) {
     propagate(node, b, e);
-    if( b > e || b > r || e < l ) return 0LL;
-    if( l <= b && e <= r ) {
+    if( b > e || b > r || e < l )
+        return 0;
+    if( l <= b && e <= r )
         return tree[ node ];
-    }
     int m = (b + e) >> 1;
-    ll p = query(node << 1, b, m);
-    ll q = query(node << 1 | 1, m + 1, e);
-    return p + q;
+    return query(node << 1, b, m) + query(node << 1 | 1, m + 1, e);
 }
 int main() {
-
+    int testcase, caseno = 1;
+    scanf("%d", &testcase);
+    while( testcase-- ) {
+        scanf("%d", &n);
+        color = 0;
+        memset(lazy, 0, sizeof lazy);
+        memset(tree, 0, sizeof tree);
+        memset(vis, 0, sizeof vis);
+        for(int i = 1; i <= n; i += 1) {
+            scanf("%d %d", &l, &r);
+            color += 1;
+            update();
+        }
+        int ans = 0;
+        for(int i = 1; i <= n + n; i += 1) {
+            l = r = i;
+            int c = query();
+            if( c && !vis[ c ] )
+                ans += 1, vis[ c ] = 1;
+        }
+        printf("Case %d: %d\n", caseno++, ans);
+    }
     return 0;
 }
