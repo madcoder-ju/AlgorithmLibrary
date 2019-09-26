48bcd808590717e025a93143a8cfdeb478616967
MasumBhuiyan <50369967+MasumBhuiyan@users.noreply.github.com>
2019-09-27 00:12:09 +0600

    Update SegmentTreeClassical.cpp

--- SegmentTreeClassical.cpp
+++ SegmentTreeClassical.cpp
@@ -1,65 +1,82 @@
+/*
+ * LightOJ 1112: Curious Robin Hood
+ * given an array A of n elements and q queries.
+ * 1 pos: print A[ pos ] && make A[ pos ] = 0
+ * 2 pos v: add v to A[ pos ]
+ * 3 l r: print sum of element in the interval [l, r]
+ * A is 0 indexed
+ */
 #include <bits/stdc++.h>
 
 using namespace std;
 
-using ll = long long;
-const int N = 1e5 + 1;
+const int N = 1e5 + 15;
 
-int n, l, r, pos, x, a[ N ], t[ N << 2 ];
+int n, l, r, pos, val, ara[ N ], tree[ N << 2 ];
 
 void construct(int node = 1, int b = 1, int e = n) {
     if( b == e ) {
-        t[ node ] = a[ b ];
+        tree[ node ] = ara[ b ];
         return ;
     }
     int m = (b + e) >> 1;
     construct(node << 1, b, m);
     construct(node << 1 | 1, m + 1, e);
-    t[ node ] = t[ node << 1 ] + t[ node << 1 | 1 ];
+    tree[ node ] = tree[ node << 1 ] + tree[ node << 1 | 1 ];
 }
-
 void update(int node = 1, int b = 1, int e = n) {
     if( b == e ) {
-        t[ node ] = x;
-        return;
+        tree[ node ] += val;
+        return ;
     }
     int m = (b + e) >> 1;
-    if( pos <= m ) update(node << 1, b, m);
-    else update(node << 1 | 1, m + 1, e);
-    t[ node ] = t[ node << 1 ] + t[ node << 1 | 1 ];
+    if( pos <= m )
+        update(node << 1, b, m);
+    else
+        update(node << 1 | 1, m + 1, e);
+    tree[ node ] = tree[ node << 1 ] + tree[ node << 1 | 1 ];
 }
-ll query(int node = 1, int b = 1, int e = n) {
-    if( b > e || b > r || e < l ) return 0;
-    if( l <= b && e <= r ) {
-        return t[ node ];
-    }
+int query(int node = 1, int b = 1, int e = n) {
+    if( b > e || b > r || e < l )
+        return 0;
+    if( l <= b && e <= r )
+        return tree[ node ];
     int m = (b + e) >> 1;
-    int P = query(node << 1, b, m);
-    int Q = query(node << 1 | 1, m + 1, e);
-    return P + Q;
+    return query(node << 1, b, m) + query(node << 1 | 1, m + 1, e);
 }
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    int testcase, caseno = 1;
+    scanf("%d", &testcase);
 
-    cin >> n;
-    for(int i = 1; i <= n; i += 1)
-        cin >> a[ i ];
-
-    construct();
-
-    int q;
-    cin >> q;
-    while( q-- ) {
-        int op;
-        cin >> op;
-        if( op == 1 ) {
-            cin >> l >> r;
-            cout << query() << '\n';
-        } else {
-            cin >> l >> r >> x;
-            update();
+    while( testcase-- ) {
+        int q;
+        scanf("%d%d", &n, &q);
+        for(int i = 1; i <= n; i += 1) {
+            scanf("%d", &ara[ i ]);
+        }
+        construct();
+        printf("Case %d:\n", caseno++);
+        while( q-- ) {
+            int op;
+            scanf("%d", &op);
+            if( op == 1 ) {
+                scanf("%d", &pos);
+                pos += 1;
+                printf("%d\n", ara[ pos ]);
+                val = -ara[ pos ];
+                ara[ pos ] = 0;
+                update();
+            } else if( op == 2 ) {
+                scanf("%d%d", &pos, &val);
+                pos += 1;
+                ara[ pos ] += val;
+                update();
+            } else {
+                scanf("%d%d", &l, &r);
+                l += 1;
+                r += 1;
+                printf("%d\n", query());
+            }
         }
     }
     return 0;
