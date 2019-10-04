263f0b443417c3ca7d3448de1639c1c400995795
MasumBhuiyan <50369967+MasumBhuiyan@users.noreply.github.com>
2019-10-04 17:27:36 +0600

    Update ChineseRemaindderTheorem.cpp

--- ChineseRemaindderTheorem.cpp
+++ ChineseRemaindderTheorem.cpp
@@ -1,32 +1,71 @@
-/*
-solves the congruence equations
-  x=r1(mod m1)
-  x=r2(mod m2)
-  x=r3(mod m3)
-  x=r4(mod m4)
-finds minimum x modulo m
-rr is the remainder array
-mm is the modulo array
-*/
-int exgcd(int a,int b,int &x,int &y)
+#include<bits/stdc++.h>
+using namespace std;
+template<class T> T egcd(T a, T b, T &x, T &y)///return ax+by=gcd(a,b)
 {
-  if(!b){x=1,y=0;return a;}
-  int x1,y1;
-  int g=exgcd(b,a%b,x1,y1);
-  x=y1;
-  y=x1-((int)(a/b))*y1;
-  return g;
+    if(b == 0)
+    {
+        x = 1, y = 0;
+        return a;
+    }
+    T x1, y1;
+    T g = egcd(b, a%b, x1, y1);
+    x = y1, y = x1-(a/b)*y1;
+    return g;
 }
-int crt(vector<int>rr,vector<int>mm)
+///Chinese remainder theorem:
+template<class T> inline T crt(vector<T>M, vector<T>A) ///gcd(mo[i],mo[j]) = 1, for any (i,j) such i!=j
 {
-  int r=rr[0],m=mm[0],f=0,n=rr.size();
-  for(int i=1;i<n;i++)
-  {
-    int x,y,g;g=exgcd(m,mm[i],x,y);
-    if((rr[i]-r)%g!=0)return -1;
-    int md=(m/g)*mm[i];
-    r=r+((((rr[i]-r)/g*x)%mm[i])*m)%md;
-    r=(r+md)%md;m=md;
-  }
-  return r;
+    /**returns {x,L}, where x is the solution unique to mod L*/
+    int sz = (int)A.size();
+    ll n = 1;
+    for(auto p : M)
+        n*=p;
+    ll res = 0;
+    for(int i = 0; i<sz; i++)
+    {
+        long long m_i= n/M[i];
+        long long m_inv, ing;
+        egcd(m_i,M[i]*1LL,m_inv,ing);
+        if(m_inv < 0)
+            m_inv += M[i];
+        res = (res + ((m_i * m_inv) % n) * A[i])%n; //be very careful with overflow
+    }
+    return res;
+}
+
+///General Version:
+/** Works for non-coprime moduli.Returns -1 if solution does not exist or input
+ is invalid.Otherwise, returns x, where x is the solution unique to mod L */
+
+typedef __int128 INT;///Test first for support __int128
+template<class T> T chinese_remainder_theorem( vector<T> M, vector<T> A )
+{
+    /**returns {x,L}, where x is the solution unique to mod L*/
+    if(A.size() != M.size())
+        return -1; /** Invalid input*/
+    int n = A.size();
+    INT a1 = A[0];
+    INT m1 = M[0];
+    /** Initially x = a_0 (mod m_0)*/
+    /** Merge the solution with remaining equations */
+    for ( int i = 1; i < n; i++ )
+    {
+        INT a2 = A[i];
+        INT m2 = M[i];
+        INT g = __gcd(m1, m2);
+        if ( a1 % g != a2 % g )
+            return -1; /** No solution exists*/
+        /** Merge the two equations*/
+        INT p, q;
+        egcd(m1/g, m2/g, p, q);
+        INT mod = (m1 / g) * m2; /** LCM of m1 and m2*/
+        /** We need to be careful about overflow, but I did not bother about overflow here to keep the code simple.*/
+        INT x = (a1*   (((m2/g)*q)%mod) + a2* (((m1/g)*p) %mod ) ) % mod;
+        /** Merged equation*/
+        a1 = x;
+        if (a1 < 0)
+            a1 += mod; /** Result is not suppose to be negative*/
+        m1 = mod;
+    }
+    return a1==0 ? m1 : a1; ///This result is for moduli m1
 }
